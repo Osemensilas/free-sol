24f434a8726829539c3de90d54d70f4cfa033aff
unknown <osemensilas@gmail.com>
2025-11-14 17:43:18 +0100

    New Commit

--- components/comonent/Machine.jsx
+++ components/comonent/Machine.jsx
@@ -20,10 +20,10 @@ const Machine = ({lang}) => {
                 })
 
                 if (response.data.status === 'success'){
-                    setSolClaim(Number(response.data.sol_recovered));
-                    setAccountsRecovered(Number(response.data.acct_recovered));
-                    setRebates(Number(response.data.rebates));
-                    setSolsPrice(Number(response.data.sols_price));
+                    setSolClaim(response.data.sol_recovered);
+                    setAccountsRecovered(response.data.acct_recovered);
+                    setRebates(response.data.rebates);
+                    setSolsPrice(response.data.sols_price);
                 }
             } catch (error) {
                 console.log("Error fetching overview: ", error);
@@ -41,16 +41,16 @@ const Machine = ({lang}) => {
                 <h3>{lang === 'en' ? 'Total $SOL Recovered' : '总回收的 $SOL'}</h3>
                 <div className="machine-row">
                     <img src="/Solana_logo.svg" alt="" />
-                    <h4>{Number(solClaimed).toLocaleString()} (${Number(0.77 * solClaimed).toLocaleString()}M)</h4>
+                    <h4>{solClaimed}</h4>
                 </div>
                 <div className="margin"></div>
                 <h3>{lang === 'en' ? 'Total Account Claimed' : '已领取的账户总数'}</h3>
-                <h4>{Number(accountsRecovered).toLocaleString()}</h4>
+                <h4>{accountsRecovered}</h4>
                 <div className="margin"></div>
                 <h3>{lang === 'en' ? 'Total Rebates' : '总返利'}</h3>
                 <div className="machine-row">
                     <img src="/Solana_logo.svg" alt="" />
-                    <h4>{Number(rebates).toLocaleString()} (${Number(0.77 * rebates).toLocaleString()}K)</h4>
+                    <h4>{rebates}</h4>
                 </div>
             </div>
         </div>
